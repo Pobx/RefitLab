2efaa5aab2d51b13fc0132c7ee21e4d728d4415b
Pobx <geidtiphong.s@allrightbox.com>
2022-03-19 15:44:32 +0700

    just remove folder not go to repository

--- Controllers/GuestController.cs
+++ Controllers/GuestController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using static IdentityModel.OidcConstants;
 
 namespace RefitLab.Controllers
 {
@@ -12,44 +13,78 @@ namespace RefitLab.Controllers
   [ApiController]
   public class GuestController : ControllerBase
   {
-    private readonly IPostApi _iPostApi;
-    public GuestController (IPostApi iPostApi)
+    private readonly IpobxApi _ipobxApi;
+    public GuestController (IpobxApi ipobxApi)
     {
-      _iPostApi = iPostApi;
+      _ipobxApi = ipobxApi;
     }
 
-    [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<PostEntity>>> getPosts ()
+    [HttpGet ("discovery")]
+    public async Task<ActionResult<string>> disCovery ()
     {
-      var client = new HttpClient ();
-      var disco = await client.GetDiscoveryDocumentAsync ("https://localhost:5001");
-      if (disco.IsError)
-      {
-        Console.WriteLine (disco.Error);
-        return NoContent ();
-      }
+      var response = await _ipobxApi.disCovery ();
+      Console.WriteLine (response);
 
-      return Ok ("");
+      return Ok (response);
+
     }
 
-    [HttpPost]
-    public async Task<ActionResult<PostEntity>> createPost ()
+    [HttpGet ("token")]
+    public async Task<object> getToken ()
     {
-      PostEntity entity = new (1, 0, "Pobx", "Hello Pobx !");
-      string token = "1234";
+      // var entity = new ClientCredentialsTokenRequest
+      // {
+      //   ClientId = "pobx",
+      //   ClientSecret = "secret1234",
+      //   Scope = "api1"
+      // };
 
-      var response = await _iPostApi.CreatePost (entity, token);
-      return Created ("", response);
+      var data = new Dictionary<string, object>
+        { { IdentityModel.OidcConstants.TokenRequest.ClientId, "pobx" },
+          { IdentityModel.OidcConstants.TokenRequest.ClientSecret, "secret1234" },
+          { IdentityModel.OidcConstants.TokenRequest.Scope, "api1" },
+          { IdentityModel.OidcConstants.TokenRequest.GrantType, GrantTypes.ClientCredentials },
+        };
+
+      var response = await _ipobxApi.getToken (data);
+      Console.WriteLine (response);
+
+      return Ok (response);
+    }
+
+    [HttpGet ("identity")]
+    public async Task<ActionResult<object>> identity ()
+    {
+      // var httpClient = new CreateHttpClient("https://localhost:6001");
+      var clientCertificateHandler = new HttpClientHandler ();
+      clientCertificateHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+      var httpClient = new HttpClient (clientCertificateHandler);
+      httpClient.BaseAddress = new Uri ("https://localhost:6001");
+
+      var token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IkQwQzQ0ODA3MTlENjFDQTU5NDkwRUYwQ0VCRkJFNkFGIiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2NDc1OTIwODQsImV4cCI6MTY0NzU5NTY4NCwiaXNzIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTAwMSIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDEvcmVzb3VyY2VzIiwiY2xpZW50X2lkIjoicG9ieCIsImp0aSI6IjgwQTA5OUExNEQ0QjQwQTNCQkJGNTgyNkNCOUQyQUZEIiwiaWF0IjoxNjQ3NTkyMDg0LCJzY29wZSI6WyJhcGkxIl19.MqVgAaqJeliGPXq8nf4JJkuBKZmyVowRDRVFgFw1mGag1keJXjJf7TBZGUX0w-_yzGl6Jim9IEkv6Xt-iSiaxyuk_ihQppQD1MVdKsXfp7B7IV2TJjnRTTq5oy9KjPUi_pA4UXnmOxFhCiAvW8XE0Ut43SmeG-rb7SLbCNg5kOZKYihhvbRzi-M4lQTENkWw8pEbbSHzemYSQl2Y1dCF777gxGbV2xl9RWwxeCyNj1LvvohggIw9F6BpzEBlxGRpJj-ByUZoEC6r0MySfS6BmdGsOs2eXnbTLZT3tmDUwZF5mpM8WundCCv7W5J6URFGk8A05OxJ-nv2uvTZ2oDFBA";
+      var api = RestService.For<IpobxApi2> (httpClient);
+      var response = await api.identity (token);
+      Console.WriteLine (response);
+
+      return Ok (response);
+
     }
   }
 
-  public interface IPostApi
+  public interface IpobxApi
   {
-    [Get ("/posts")]
-    Task<IReadOnlyList<PostEntity>> GetPosts ();
 
-    [Post ("/posts")]
-    Task<PostEntity> CreatePost (PostEntity entity, [Authorize ("Bearer")] string token);
+    [Post ("/connect/token")]
+    Task<object> getToken ([Body (BodySerializationMethod.UrlEncoded)] Dictionary<string, object> data);
+
+    [Get ("/.well-known/openid-configuration")]
+    Task<object> disCovery ();
+  }
+
+  public interface IpobxApi2
+  {
+    [Get ("/identity")]
+    Task<object> identity ([Authorize ("Bearer")] string token);
   }
 
   public record PostEntity (int userId, int id, string title, string body);
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,12 +9,19 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Refit;
 using RefitLab.Controllers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 
 namespace RefitLab
 {
   public class Startup
   {
-    public Startup(IConfiguration configuration)
+    public Startup (IConfiguration configuration)
     {
       Configuration = configuration;
     }
@@ -26,37 +29,47 @@ namespace RefitLab
     public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
+    public void ConfigureServices (IServiceCollection services)
     {
 
-      services.AddControllers();
-      services.AddSwaggerGen(c =>
+      services.AddControllers ();
+      services.AddSwaggerGen (c =>
       {
-        c.SwaggerDoc("v1", new OpenApiInfo { Title = "RefitLab", Version = "v1" });
+        c.SwaggerDoc ("v1", new OpenApiInfo { Title = "RefitLab", Version = "v1" });
       });
 
-      services.AddRefitClient<IPostApi>().ConfigureHttpClient(c => c.BaseAddress = new Uri("https://jsonplaceholder.typicode.com"));
+      // Console.WriteLine(Path.Combine (Directory.GetCurrentDirectory(), "ssl/https.pfx"));
+      // Console.WriteLine(Directory.GetCurrentDirectory());
+      var clientCertificateHandler = new HttpClientHandler ();
+      // clientCertificateHandler.ClientCertificates.Add (new X509Certificate2 (Path.Combine (Directory.GetCurrentDirectory (), "ssl/https.pfx"), "1234"));
+      clientCertificateHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+      services.AddRefitClient<IpobxApi> ().ConfigureHttpClient (c => c.BaseAddress = new Uri ("https://localhost:5001"))
+        .ConfigurePrimaryHttpMessageHandler (_ => clientCertificateHandler);
+      // .ConfigurePrimaryHttpMessageHandler (_ => new HttpClientHandler
+      // {
+      //   ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; }
+      // });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure (IApplicationBuilder app, IWebHostEnvironment env)
     {
-      if (env.IsDevelopment())
+      if (env.IsDevelopment ())
       {
-        app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RefitLab v1"));
+        app.UseDeveloperExceptionPage ();
+        app.UseSwagger ();
+        app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "RefitLab v1"));
       }
 
-      app.UseHttpsRedirection();
+      app.UseHttpsRedirection ();
 
-      app.UseRouting();
+      app.UseRouting ();
 
-      app.UseAuthorization();
+      app.UseAuthorization ();
 
-      app.UseEndpoints(endpoints =>
+      app.UseEndpoints (endpoints =>
       {
-        endpoints.MapControllers();
+        endpoints.MapControllers ();
       });
     }
   }
