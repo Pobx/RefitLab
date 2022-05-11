17b87a15ca371095a880371332904d50758121bc
Pobx <geidtiphong@gmail.com>
2022-05-11 20:01:41 +0700

    just coding for show certificate

--- Startup.cs
+++ Startup.cs
@@ -20,90 +20,98 @@ using System.Threading.Tasks;
 
 namespace RefitLab
 {
-  public class Startup
-  {
-    public Startup (IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RefitLab", Version = "v1" });
+            });
+
+            // Console.WriteLine(Path.Combine (Directory.GetCurrentDirectory(), "ssl/https.pfx"));
+            // Console.WriteLine(Directory.GetCurrentDirectory());
+            // X509Certificate x509 = X509Certificate.CreateFromCertFile (@"ssl/dev.cer");
+            // The path to the certificate.
+            // string Certificate = "ssl/dev.cer";
+
+            // // Load the certificate into an X509Certificate object.
+            // var cert = new X509Certificate2(X509Certificate.CreateFromCertFile(Certificate));
+
+            // // Get the value.
+            // string resultsTrue = cert.ToString(true);
+
+            // // Display the value to the console.
+            // Console.WriteLine(resultsTrue);
+
+            // // Get the value.
+            // string resultsFalse = cert.ToString(false);
+
+            // // Display the value to the console.
+            // Console.WriteLine(resultsFalse);
+
+            // var clientCertificateHandler = new HttpClientHandler();
+            // clientCertificateHandler.ClientCertificates.Add(cert);
+            // services.AddRefitClient<IpobxApi>().ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7090"));
+            // .ConfigurePrimaryHttpMessageHandler (_ => clientCertificateHandler);
+
+            // var clientCertificateHandler = new HttpClientHandler ();
+            // // clientCertificateHandler.ClientCertificates.Add (new X509Certificate2 (Path.Combine (Directory.GetCurrentDirectory (), "ssl/https.pfx"), "1234"));
+            // clientCertificateHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            // services.AddRefitClient<IpobxApi> ().ConfigureHttpClient (c => c.BaseAddress = new Uri ("https://localhost:5002"))
+            //   .ConfigurePrimaryHttpMessageHandler (_ => clientCertificateHandler);
+
+            services.AddRefitClient<IpobxApi>().ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:7090"))
+            .ConfigurePrimaryHttpMessageHandler(_ => new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) =>
+                {
+                    Console.WriteLine($"sender ===> {sender}");
+                    Console.WriteLine($"cert ===> {cert}");
+                    Console.WriteLine($"chain ===> {chain}");
+                    Console.WriteLine($"sslPolicyErrors ===> {sslPolicyErrors}");
+                    return true;
+                }
+            });
+
+            // services.AddAuthentication ()
+            //   .AddCookie (CookieAuthenticationDefaults.AuthenticationScheme + "-CreatedByPobx", options =>
+            //   {
+            //     options.ExpireTimeSpan = TimeSpan.FromMinutes (5);
+            //     options.SlidingExpiration = true;
+            //   });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RefitLab v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
     }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices (IServiceCollection services)
-    {
-
-      services.AddControllers ();
-      services.AddSwaggerGen (c =>
-      {
-        c.SwaggerDoc ("v1", new OpenApiInfo { Title = "RefitLab", Version = "v1" });
-      });
-
-      // Console.WriteLine(Path.Combine (Directory.GetCurrentDirectory(), "ssl/https.pfx"));
-      // Console.WriteLine(Directory.GetCurrentDirectory());
-      // X509Certificate x509 = X509Certificate.CreateFromCertFile (@"ssl/dev.cer");
-      // The path to the certificate.
-      string Certificate = "ssl/dev.cer";
-
-      // Load the certificate into an X509Certificate object.
-      var cert = new X509Certificate2 (X509Certificate.CreateFromCertFile (Certificate));
-
-      // Get the value.
-      string resultsTrue = cert.ToString (true);
-
-      // Display the value to the console.
-      Console.WriteLine (resultsTrue);
-
-      // Get the value.
-      string resultsFalse = cert.ToString (false);
-
-      // Display the value to the console.
-      Console.WriteLine (resultsFalse);
-
-      var clientCertificateHandler = new HttpClientHandler ();
-      clientCertificateHandler.ClientCertificates.Add (cert);
-      services.AddRefitClient<IpobxApi> ().ConfigureHttpClient (c => c.BaseAddress = new Uri ("https://localhost:5002"))
-        .ConfigurePrimaryHttpMessageHandler (_ => clientCertificateHandler);
-
-      // var clientCertificateHandler = new HttpClientHandler ();
-      // // clientCertificateHandler.ClientCertificates.Add (new X509Certificate2 (Path.Combine (Directory.GetCurrentDirectory (), "ssl/https.pfx"), "1234"));
-      // clientCertificateHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
-      // services.AddRefitClient<IpobxApi> ().ConfigureHttpClient (c => c.BaseAddress = new Uri ("https://localhost:5002"))
-      //   .ConfigurePrimaryHttpMessageHandler (_ => clientCertificateHandler);
-
-      // .ConfigurePrimaryHttpMessageHandler (_ => new HttpClientHandler
-      // {
-      //   ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; }
-      // });
-
-      // services.AddAuthentication ()
-      //   .AddCookie (CookieAuthenticationDefaults.AuthenticationScheme + "-CreatedByPobx", options =>
-      //   {
-      //     options.ExpireTimeSpan = TimeSpan.FromMinutes (5);
-      //     options.SlidingExpiration = true;
-      //   });
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure (IApplicationBuilder app, IWebHostEnvironment env)
-    {
-      if (env.IsDevelopment ())
-      {
-        app.UseDeveloperExceptionPage ();
-        app.UseSwagger ();
-        app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "RefitLab v1"));
-      }
-
-      app.UseHttpsRedirection ();
-
-      app.UseRouting ();
-
-      app.UseAuthorization ();
-
-      app.UseEndpoints (endpoints =>
-      {
-        endpoints.MapControllers ();
-      });
-    }
-  }
 }
